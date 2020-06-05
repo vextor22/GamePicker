dd1618e15b4c0f0aa15f51bb1b429162ac1d885c
Matt Higgins <matt@matthiggins.dev>
2020-06-04 22:17:03 -0400

    Add steam URL parsing

--- web/gamepicker/src/components/UserSubmit.vue
+++ web/gamepicker/src/components/UserSubmit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row justify-content-sm-center">
+  <div class="row justify-content-center">
     <div class="col-sm-6">
       <div class="steamid-input card">
         <form>
@@ -7,12 +7,16 @@
             <label for="SteamID">SteamID</label>
             <input
               type="text"
-              class="form-control"
+              :class="{'form-control': true, 'is-invalid': warningText}"
               id="SteamID"
               aria-describedby="steamID Input"
               placeholder="Enter SteamID"
-              v-model="steamID"
+              @input="userChanged"
             />
+            <small class="text-danger" v-if="warningText">{{ warningText }}</small>
+            <small v-if="steamID">Potential ID: {{ steamID }}</small>
+          </div>
+          <div class="form-group" style="width: 75%; margin: auto;">
             <button type="submit" @click.prevent="submitted" class="btn btn-primary">Submit SteamID</button>
           </div>
         </form>
@@ -25,13 +29,39 @@
 export default {
   data() {
     return {
-      steamID: ""
+      steamID: "",
+      warningText: ""
     };
   },
   methods: {
     submitted(e) {
       console.log("Clicked me", this.steamID);
       this.$emit("submitSteamID", this.steamID);
+    },
+    userChanged(e) {
+      var userID = e.target.value;
+      if (userID.includes("steamcommunity")) {
+        var splits = userID.split("/");
+        for (const [i, value] of splits.entries()) {
+          console.log("%d: %s", i, value);
+          if (value === "profiles" || value === "id") {
+            var possibleID = splits[i + 1];
+            if (possibleID) {
+              this.warningText = "";
+              this.steamID = splits[i + 1];
+              console.log("ID Found: %s", this.steamID);
+              return;
+            }
+          }
+        }
+        this.steamID = "";
+        this.warningText = "I haven't found a user ID in this URL";
+      } else if (userID !== "") {
+        this.steamID = userID;
+      } else {
+        this.steamID = "";
+        this.warningText = "";
+      }
     }
   }
 };
